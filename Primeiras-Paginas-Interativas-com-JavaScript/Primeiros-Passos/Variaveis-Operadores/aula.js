246301824765392a86a523ef5ca9b0515c6dc1b0
Leandro Rosendo <leo.rosendo@gmail.com>
2022-10-27 16:52:51 -0300

    Primeira pratica com javascript

---
+++ Primeiras-Paginas-Interativas-com-JavaScript/Primeiros-Passos/Variaveis-Operadores/aula.js
@@ -0,0 +1,10 @@
+
+
+
+const consumoMedio = 10;
+let distancia = 5000;
+const valorCombustivel = 4.8;
+
+totalGasto = (distancia/consumoMedio) * valorCombustivel;
+
+console.log(totalGasto)
---
+++ Primeiras-Paginas-Interativas-com-JavaScript/Primeiros-Passos/Variaveis-Operadores/primeiro-desafio.js
@@ -0,0 +1,16 @@
+/*Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+1- Preço do combustivel;
+2- Gasto médio de combustível do carro por KM;
+3- Distância em KM da viagem.
+*/
+
+const precoCombustivel = 5.79;
+const consumoPorKM = 10;
+const distanciaTotalemKM = 100;
+let gastoTotalViagem = 0;
+
+gastoTotalViagem = (distanciaTotalemKM/consumoPorKM)*precoCombustivel;
+
+console.log (gastoTotalViagem);
