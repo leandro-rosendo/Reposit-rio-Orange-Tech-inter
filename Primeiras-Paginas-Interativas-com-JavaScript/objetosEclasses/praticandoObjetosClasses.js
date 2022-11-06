9f6c2c02d0cda12bd95368a22b1df1e2ea6c56c5
Leandro Rosendo <leo.rosendo@gmail.com>
2022-11-06 08:39:34 -0300

    Praticndo conhecimentos em objeto e classe

---
+++ Primeiras-Paginas-Interativas-com-JavaScript/objetosEclasses/praticandoObjetosClasses.js
@@ -0,0 +1,36 @@
+/*Crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por kilometro rodado.
+Crie um método que dado a quantidade de quilometros e o preço do combustível nos dê o valor gasto em reais para realizar este percurso.*/
+
+//
+class Carro {
+    marcaDoCarro;
+    corDoCarro;
+    gastoMedioPorKilometro;
+
+    constructor (marca, cor,  gastoMedio){
+        this.marcaDoCarro = marca;
+        this.corDoCarro = cor;
+        this.gastoMedioPorKilometro = gastoMedio;
+    }
+
+    calcularGastoMedioDaViagem(distancia, precoCombustivel){
+        return (distancia/this.gastoMedioPorKilometro)*precoCombustivel;
+    }
+}
+
+/*function gastomeidoCombustivel(){
+    const precoGasolina= 4.80;
+    const distanciaDaViagem= 150;
+    
+    custoDaViagem = (distanciaDaViagem/10)*precoGasolina;
+
+    return  custoDaViagem;
+
+}*
+custo = gastomeidoCombustivel();*/
+
+const celta = new Carro( 'Volksvagem', 'azul', 10);
+custo = celta.calcularGastoMedioDaViagem(180, 4.85);
+
+console.log('O nome do carro é:',celta.marcaDoCarro+'. A cor do Carro é:',celta.corDoCarro+'. O custo da viagem será de: R$=',custo .toFixed(2));
---
+++ Primeiras-Paginas-Interativas-com-JavaScript/praticando-com-variaveis-operadores-conicionais/calcula-desconto.js
@@ -0,0 +1,24 @@
+/*Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o clculo adequado.
+
+Código condição de pagamento:
+1- À vista débito, recebe 10% de desconto;
+2- À vista no dinheiro ou PIX, recebe 15% de desconto;
+3- Em duas vezes, preço normal de etiqueta sem juros;
+4- Acima de duas vezes, preço normal de etiqueta mais juros de 10%.*/
+
+//Declaração de variáveis contantes
+
+const precoEtiqueta = 100;
+const precoFinal = 0;
+const opcaoPagamento1= 1;
+const debito = 10;
+const acimaDuasVezesCartao = 10;
+const duasVezesCartao = 0;
+
+duasVezesCartao = precoProduto;
+
+let opcao1 = 
+
+//Compra com dinheiro ou PIX
+ if ()
