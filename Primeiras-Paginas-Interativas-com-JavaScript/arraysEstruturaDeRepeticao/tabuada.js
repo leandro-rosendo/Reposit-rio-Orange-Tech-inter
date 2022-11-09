83295ce899e1b6decaaf7da129b527c36f804816
Leandro Rosendo <leo.rosendo@gmail.com>
2022-11-08 21:36:09 -0300

    Programa que cria uma tabuada

---
+++ Primeiras-Paginas-Interativas-com-JavaScript/arraysEstruturaDeRepeticao/tabuada.js
@@ -0,0 +1,9 @@
+//Programa que recebe um número qualquer e imprime a tabuada
+
+let numero = 5;//Número dado
+
+//Estrutura de repetição que cria a tabuada
+for (i=1; i <= 10; i++ ){
+    let resultado = numero*i;
+    console.log(resultado);
+}
